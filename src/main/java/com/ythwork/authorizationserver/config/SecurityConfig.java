323814e1567d941ec15aa3c43fe183b6b84ac2e7
yangtaehwan <ythwork83@gmail.com>
2026-04-11 20:26:07 +0900

    remove unused import

--- src/main/java/com/ythwork/authorizationserver/config/SecurityConfig.java
+++ src/main/java/com/ythwork/authorizationserver/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointR
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
